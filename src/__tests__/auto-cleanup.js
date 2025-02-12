12aa8ab1bef885781affd3576dcc4af198f09610
stivencabrera3 <stiven.cabrera3@gmail.com>
2025-02-11 20:43:06 -0500

    test: add test coverage for multi-root and slot cleanup (#295)

--- src/__tests__/auto-cleanup.js
+++ src/__tests__/auto-cleanup.js
@@ -16,3 +16,33 @@ test('renders the component', () => {
 test('cleans up after each test by default', () => {
   expect(document.body.innerHTML).toMatchInlineSnapshot(``)
 })
+
+test('renders multi-root component', () => {
+  render({
+    template: `
+      <h1>Hello World</h1>
+      <h2>Hello World</h2>
+    `,
+  })
+
+  expect(document.body.innerHTML).toMatchInlineSnapshot(`
+    <div>
+      <h1>Hello World</h1>
+      <h2>Hello World</h2>
+    </div>
+  `)
+})
+
+test('cleans up after rendering multi-root node', () => {
+  expect(document.body.innerHTML).toMatchInlineSnapshot(``)
+})
+
+test('renders single slot component', () => {
+  render({template: `<slot />`})
+
+  expect(document.body.innerHTML).toMatchInlineSnapshot(`<div></div>`)
+})
+
+test('cleans up after rendering slot component', () => {
+  expect(document.body.innerHTML).toMatchInlineSnapshot(``)
+})
